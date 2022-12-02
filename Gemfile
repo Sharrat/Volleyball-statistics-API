dbfb88ddf3eb0b73b0de5693b60d0665e1f3f08a
Artek58 <artur.chrapek3@gmail.com>
2022-12-02 22:33:25 +0100

    tescik

--- Gemfile
+++ Gemfile
@@ -29,3 +29,4 @@ group :development do
   gem 'faker'
 end
 
+#testtt
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,5 @@
 class Team < ApplicationRecord
   validates :Team_name, presence: true
   validates :Shortened_team_name, presence: true
+  #
 end
