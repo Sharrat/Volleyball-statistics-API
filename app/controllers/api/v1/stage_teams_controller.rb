45a371d56468f61c45a1664483e69fd32427b5a2
Swjan <swjan0@gmail.com>
2022-12-27 15:30:53 +0100

    Stage_teams controller exception handling

--- app/controllers/api/v1/stage_teams_controller.rb
+++ app/controllers/api/v1/stage_teams_controller.rb
@@ -8,31 +8,50 @@ module Api
       end
 
       def show
-        stage_team = StageTeam.find(params[:id])
-        render json: {status: 'SUCCESS', message:'Loaded stage team', data:stage_team},status: :ok
+        if StageTeam.exists?(params[:id])
+          stage_team = StageTeam.find(params[:id])
+          render json: {status: 'SUCCESS', message:'Loaded stage team', data:stage_team},status: :ok
+        else
+          render json: {status: 'ERROR', message:'Stage team not loaded', data:{"id": ["must exist"]}},status: :not_found
+        end
       end
 
       def create
-        stage_team = StageTeam.new(stage_team_params)
-          if stage_team.save
-            render json: {status: 'SUCCESS', message:'Saved stage team', data:stage_team},status: :ok
-          else
-            render json: {status: 'ERROR', message:'Stage team not saved', data:stage_team.errors},status: :unprocessable_entity
-          end
+        if TournamentStage.exists?(params[:tournament_stage_id]) && Team.exists?(params[:team_id])
+          stage_team = StageTeam.new(stage_team_params)
+            if stage_team.save
+              render json: {status: 'SUCCESS', message:'Saved stage team', data:stage_team},status: :ok
+            else
+              render json: {status: 'ERROR', message:'Stage team not saved', data:stage_team.errors},status: :unprocessable_entity
+            end
+        else
+          render json: {status: 'ERROR', message:'Stage team not saved', data:{"tournament_stage": ["must exist"], "team": ["must exist"]}},status: :not_found
+        end
       end
 
       def destroy
-        stage_team = StageTeam.find(params[:id])
-        stage_team.destroy
-        render json: {status: 'SUCCESS', message:'Deleted stage team', data:stage_team},status: :ok
+        if StageTeam.exists?(params[:id])
+          stage_team = StageTeam.find(params[:id])
+          if stage_team.destroy
+            render json: {status: 'SUCCESS', message:'Deleted stage team', data:stage_team},status: :ok
+          else
+            render json: {status: 'ERROR', message:'Stage team not deleted', data:stage_team.errors}, status: :unprocessable_entity
+          end
+        else
+          render json: {status: 'ERROR', message:'Stage team not deleted', data:{"id": ["must exist"]}},status: :not_found
+        end
       end
 
       def update
-        stage_team = StageTeam.find(params[:id])
-        if stage_team.update(stage_team_params)
-          render json: {status: 'SUCCESS', message:'Updated stage team', data:stage_team},status: :ok
+        if StageTeam.exists?(params[:id])
+          stage_team = StageTeam.find(params[:id])
+          if stage_team.update(stage_team_params)
+            render json: {status: 'SUCCESS', message:'Updated stage team', data:stage_team},status: :ok
+          else
+            render json: {status: 'ERROR', message:'Stage team not updated', data:stage_team.errors},status: :unprocessable_entity
+          end
         else
-          render json: {status: 'ERROR', message:'Stage team not updated', data:stage_team.errors},status: :unprocessable_entity
+          render json: {status: 'ERROR', message:'Stage team not deleted', data:{"id": ["must exist"]}},status: :not_found
         end
       end
 
